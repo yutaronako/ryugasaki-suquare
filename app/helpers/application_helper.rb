e1a5ea64c042e8527b5243d629c83830b0f1d8ab
yutaronako <nakonakoshi@gmail.com>
2020-05-31 05:34:45 +0000

    responsive4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+    # ページごとにタイトルを設定
+    def full_title(page_title = "")
+        base_title = "タツノコスクエア"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
+    
 end
